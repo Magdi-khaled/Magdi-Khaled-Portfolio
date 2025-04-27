c557cc72c1a77cd319cd629e8cd58c17071f63ad
Magdi-Khaled <migokhaled88@gmail.com>
2025-04-27 22:45:26 +0300

    feat(content): full code, update nav item (hover)

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -8,10 +8,13 @@ defineProps({
 </script>
 <template>
     <li>
-        <a :href="href" class="relative capitalize font-bold flex flex-col items-center gap-2 link-underline" :class="{
-            'link-underline-active before:bg-transparent before:md:bg-body-color': activeSection === id,
-            'before:bg-transparent before:md:bg-primary': !isDarkTheme
-        }">
+        <a :href="href"
+            class="relative capitalize font-bold flex flex-col items-center gap-2 link-underline transition-colors duration-150"
+            :class="{
+                'link-underline-active before:bg-transparent before:md:bg-body-color': activeSection === id,
+                'before:bg-transparent before:md:bg-primary hover:text-[#0000007f]': !isDarkTheme,
+                'hover:text-[#8a8787da]': isDarkTheme
+            }">
             <slot />
         </a>
     </li>
