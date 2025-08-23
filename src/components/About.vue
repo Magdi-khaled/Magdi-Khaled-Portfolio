864c01ece992103e79a93aab2590a1c2d5944ec8
Magdi-khaled <migokhaled88@gmail.com>
2025-08-23 15:07:38 +0300

    Fix: perv-image style

--- src/components/About.vue
+++ src/components/About.vue
@@ -15,7 +15,7 @@ const downloadResume = () => {
 <template>
     <div class="mt-8 flex flex-col lg:flex-row gap-4 justify-between xl:justify-center items-center">
         <div class="w-full sm:w-1/2 flex justify-center">
-            <img class="hidden sm:block rounded-4xl border-[1px] border-neutral-400 grayscale-[0.25] w-[15rem] lg:w-[18.25rem] h-[16rem] lg:h-[20em]"
+            <img class="hidden sm:block rounded-4xl border-[1px] border-neutral-400 grayscale-[0.15] w-[15rem] lg:w-[18.25rem] h-[16rem] lg:h-[20em]"
                 loading="lazy" src="@/assets/media/personal-image.webp" alt="graduate-img">
         </div>
         <div class="w-full sm:w-10/12 md:w-8/12 lg:w-6/12 flex flex-col gap-4">
@@ -43,7 +43,7 @@ const downloadResume = () => {
                     <BxSupport class="text-2xl" />
 
                     <h1 class="capitalize text-sm">opportunities</h1>
-                    <p class="text-[10px] font-medium" :class="{ 'text-secondary': !isDarkTheme }">Works at HIGHBASE</p>
+                    <p class="text-[10px] font-medium" :class="{ 'text-secondary': !isDarkTheme }">Works @ HIGHBASE</p>
                 </div>
             </div>
             <p class=" my-4 text-md w-full xl-w-11/12" :class="{ 'text-secondary': !isDarkTheme }">
